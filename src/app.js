8980a60e78785b2cb291b6ec68a67dc8f7101cee
mcestuya <93035929+mcestuya@users.noreply.github.com>
2022-01-01 23:41:18 +0000

    added alt attribute

--- src/app.js
+++ src/app.js
@@ -41,6 +41,7 @@ function displayTemperature(response) {
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
 let apiKey = "ba961258607b8a685407ad6954262a1c";
